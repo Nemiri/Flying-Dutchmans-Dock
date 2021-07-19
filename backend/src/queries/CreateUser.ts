af22bd54f0881c75d0001c562e160e17e5da48ae
Davi Banfi Nogueira <davibanfi1579@outlook.com>
2021-07-19 13:47:57 -0300

    Migrations completed
    Co-authored-by: Pedro Bonfim Oliveira <Nemiri@users.noreply.github.com>

--- backend/src/queries/CreateUser.ts
+++
@@ -1,3 +0,0 @@
-import pool from "../CreatePool";
-
-pool.query("CREATE TABLE ship (id INTEGER, name VARCHAR);");
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import { app } from "./app";
-import pool from "./CreatePool";
+import runMigrations from './queries/migrations';
 
 app.listen(3333, () => {
     console.log("Server is running on port 3333.");
 });
+
+runMigrations();
